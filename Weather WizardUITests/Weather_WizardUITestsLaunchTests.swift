57c5b492712c31774fca0f9bc38755ce251e9468
Rajat Tiwari <rajattiwari1082@gmail.com>
2026-06-12 22:25:49 +0530

    Initial Commit

---
+++ Weather WizardUITests/Weather_WizardUITestsLaunchTests.swift
@@ -0,0 +1,35 @@
+//
+//  Weather_WizardUITestsLaunchTests.swift
+//  Weather WizardUITests
+//
+//  Created by Rajat Tiwari on 12/06/26.
+//
+
+import XCTest
+
+final class Weather_WizardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+        // XCUIAutomation Documentation
+        // https://developer.apple.com/documentation/xcuiautomation
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
